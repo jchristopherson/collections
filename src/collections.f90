7fa4579f00533f021d67a12614d06900b75f2456
jchristopherson <jchristopherson@hotmail.com>
2022-12-01 13:09:15 -0600

    Add comments

--- src/collections.f90
+++ src/collections.f90
@@ -409,6 +409,82 @@ module collections
 
 ! ------------------------------------------------------------------------------
     !> Defines a generic, linked-list container.
+    !!
+    !! @par Example
+    !! The following example illustrates basic usage of the linked_list.
+    !! @code{.f90}
+    !! program linked_list_example
+    !!     use collections
+    !!     use iso_fortran_env
+    !!     implicit none
+    !!
+    !!     ! Variables
+    !!     integer(int32), parameter :: n = 10
+    !!     integer(int32) :: i
+    !!     logical :: check
+    !!     type(linked_list) :: x
+    !!     class(*), pointer :: ptr
+    !!
+    !!     ! Create a list
+    !!     do i = 1, n
+    !!         call x%push(i)
+    !!     end do
+    !!
+    !!     ! Print it out
+    !!     print '(A)', "***** Original List *****"
+    !!     check = .true.
+    !!     do while (check)
+    !!         ptr => x%get()
+    !!
+    !!         ! The list uses unlimited polymorphic types; therefore, we need to
+    !!         ! use the select type construct.
+    !!         select type (ptr)
+    !!         type is (integer(int32))
+    !!             print *, ptr
+    !!         end select
+    !!
+    !!         ! Move to the next item
+    !!         check = x%next()
+    !!     end do
+    !!
+    !!     ! Print out the item at the current iterator position
+    !!     print '(A)', new_line('a') // "***** Current Iterator Position *****"
+    !!     ptr => x%get()
+    !!     select type(ptr)
+    !!     type is (integer(int32))
+    !!         print *, ptr
+    !!     end select
+    !!
+    !!     ! Move to the beginning of the collection
+    !!     print '(A)', new_line('a') // "***** Beginning *****"
+    !!     call x%move_to_first()
+    !!     ptr => x%get()
+    !!     select type (ptr)
+    !!     type is (integer(int32))
+    !!         print *, ptr
+    !!     end select
+    !! end program
+    !! @endcode
+    !! The above program generates the following output.
+    !! @code{.txt}
+    !! ***** Original List *****
+    !!     1
+    !!     2
+    !!     3
+    !!     4
+    !!     5
+    !!     6
+    !!     7
+    !!     8
+    !!     9
+    !!    10
+    !!
+    !! ***** Current Iterator Position *****
+    !!    10
+    !!
+    !! ***** Beginning *****
+    !!     1
+    !! @endcode
     type linked_list
         ! The number of nodes in the container.
         integer(int32), private :: m_count = 0
